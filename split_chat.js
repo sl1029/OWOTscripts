2a31a18271a67680b61a6ea090e43b2a9ca55361
sl1029 <94966830+sl1029@users.noreply.github.com>
2025-10-09 22:41:05 +0300

    Update split_chat.js

--- split_chat.js
+++ split_chat.js
@@ -29,7 +29,7 @@ function rct(inputElm) {
     inputElm.value = "";
 }
 
-if (typeof onNewChatTab !== "function") { 
+if (typeof onNewChatTab() !== "function") { 
     function onNewChatTab() {
         // custom code runs here for easy cross-script implementation
         return;
